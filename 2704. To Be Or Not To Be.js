a034f0d4f88723c8eab985337f0ef6dde107a51b
Vinu Paul <vinupaul9999@gmail.com>
2024-03-09 11:05:45 -0500

    New Start

---
+++ 2704. To Be Or Not To Be.js
@@ -0,0 +1,28 @@
+// 2704. To Be Or Not To Be
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    let object = {
+    toBe: function(val1) {
+        if (val===val1){
+            return true;
+        }
+        else  throw new Error("Not Equal");
+    },
+    notToBe: function(val2) {
+         if (val!==val2){
+            return true;
+        }
+        else  throw new Error("Equal");
+    }
+};
+return object
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
