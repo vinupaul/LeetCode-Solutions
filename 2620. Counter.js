cfa9a0353857f0fe3d49a0ab41b317a4767dd87b
Vinu Paul <vinupaul9999@gmail.com>
2024-03-09 10:11:55 -0500

    New Start

---
+++ 2620. Counter.js
@@ -0,0 +1,19 @@
+// 2620. Counter
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+            return n++
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
